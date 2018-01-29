5732c7b09ac1acbdd454775b9d09c4640d76ab61
Bojan <new@mail.com>
2018-01-29 23:06:38 +0100

    Termini od 21 do 29 u jednoj vezbi Termin 29b

---
+++ app/src/main/java/com/example/androiddevelopment/glumcilegende/GenericFileProvider.java
@@ -0,0 +1,12 @@
+package com.example.androiddevelopment.glumcilegende;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by BBLOJB on 29.1.2018..
+ */
+
+public class GenericFileProvider extends FileProvider {
+
+    public static final String MY_PROVIDER = "com.example.androiddevelopment.glumcilegende.provider";
+}
--- app/src/main/java/com/example/androiddevelopment/glumcilegende/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/glumcilegende/MainActivity.java
@@ -1,108 +1,87 @@
 package com.example.androiddevelopment.glumcilegende;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 /**
  * Created by BBLOJB on 23.1.2018..
  */
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity {
 
-    SensorManager sensorManager;
-
-    TextView tvAccelerometar;
-    TextView tvLinearAccelerometar;
-    TextView tvMagneticField;
-    TextView tvProximitySensor;
-    TextView tvGyroscope;
+    private Button takePictureButton;
+    private ImageView imageView;
+    private Uri file;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        takePictureButton = (Button) findViewById(R.id.button_image);
+        imageView = (ImageView) findViewById(R.id.imageview);
 
-        tvAccelerometar = (TextView) findViewById(R.id.tvAccelerometar);
-        tvLinearAccelerometar = (TextView) findViewById(R.id.tvLinearAccelerometer);
-        tvMagneticField = (TextView) findViewById(R.id.tvMagneticField);
-        tvProximitySensor = (TextView) findViewById(R.id.tvProximitySensor);
-        tvGyroscope = (TextView) findViewById(R.id.tvGyroscope);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+
+        }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        // register this class as a listener for the orientation and
-        // accelerometer sensors
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_NORMAL);
-
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
-
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_NORMAL);
-
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            float[] values = event.values;
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
-
-            tvAccelerometar.setText("Accelerometer: [" + x + ", " + y + ", " + z + "]");
-        }else if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
-            float[] values = event.values;
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
-
-            tvLinearAccelerometar.setText("Non-gravity accelerometer: [" + x + ", " + y + ", " + z + "]");
-        }else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-            float[] values = event.values;
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
-
-            tvMagneticField.setText("Magnetic field: [" + x + ", " + y + ", " + z + "]");
-        }else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
-            float[] values = event.values;
-            float x = values[0];
-            float y = values[1];
-            float z = values[2];
-
-            tvGyroscope.setText("Gyroscpe : [" + x + ", " + y + ", " + z + "]");
-        }else if (event.sensor.getType() == Sensor.TYPE_PROXIMITY){
-            float[] values = event.values;
-            float x = values[0];
-
-            if (x>0){
-                tvProximitySensor.setText("Daleko");
-            }else {
-                tvProximitySensor.setText("Blizu");
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                takePictureButton.setEnabled(true);
             }
         }
+    }
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+    }
+    public void takePicture(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
+        file = FileProvider.getUriForFile(this, GenericFileProvider.MY_PROVIDER, getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        startActivityForResult(intent, 100);
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100){
+            if (resultCode == RESULT_OK){
+                imageView.setImageURI(file);
+            }
+        }
     }
 }
